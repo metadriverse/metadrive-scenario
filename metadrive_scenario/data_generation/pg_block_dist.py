b38c676ddf71ce3cef0d975f9f807bffcab647ec
QuanyiLi <quanyili0057@gmail.com>
2022-10-29 16:58:03 +0100

    DataSetGenerationConfig

--- metadrive_scenario/data_generation/pg_block_dist.py
+++ metadrive_scenario/data_generation/pg_block_dist.py
@@ -1,31 +1,34 @@
-from metadrive.component.algorithm.blocks_prob_dist import PGBlockConfig
+from metadrive.component.algorithm.blocks_prob_dist import PGBlockDistConfig
+from metadrive.component.pgblock.bidirection import Bidirection
 from metadrive.component.pgblock.bottleneck import Merge, Split
 from metadrive.component.pgblock.curve import Curve
 from metadrive.component.pgblock.fork import InFork, OutFork
 from metadrive.component.pgblock.parking_lot import ParkingLot
 from metadrive.component.pgblock.ramp import InRampOnStraight, OutRampOnStraight
 from metadrive.component.pgblock.roundabout import Roundabout
-from metadrive.component.pgblock.std_intersection import StdInterSection
-from metadrive.component.pgblock.std_t_intersection import StdTInterSection
+from metadrive.component.pgblock.intersection import InterSection
+from metadrive.component.pgblock.t_intersection import TInterSection
 from metadrive.component.pgblock.straight import Straight
 from metadrive.component.pgblock.tollgate import TollGate
-from metadrive.component.pgblock.bidirection import Bidirection
 
 
-class DatasetPGBlockConfig(PGBlockConfig):
+class DatasetPGBlockConfig(PGBlockDistConfig):
+    MAX_LANE_NUM = 4
+    MIN_LANE_NUM = 2
+
     BLOCK_TYPE_DISTRIBUTION_V1 = None  # set to null
     BLOCK_TYPE_DISTRIBUTION_V2 = None  # set to null
 
-    BLOCK_TYPE_DISTRIBUTION_V3 = {
+    BLOCK_TYPE_DISTRIBUTION_DATASET = {
         # 0.3 for curves
-        Curve: 0.1,
+        Curve: 0.2,
         # 0.3 for straight
         Straight: 0.1,
         InRampOnStraight: 0.1,
         OutRampOnStraight: 0.1,
         # 0.3 for intersection
-        StdInterSection: 0.1,
-        StdTInterSection: 0.1,
+        InterSection: 0.1,
+        TInterSection: 0.1,
         # 0.1 for roundabout
         Roundabout: 0.1,
         InFork: 0.00,
@@ -36,3 +39,22 @@ class DatasetPGBlockConfig(PGBlockConfig):
         TollGate: 0.00,
         Bidirection: 0.00
     }
+
+    @classmethod
+    def all_blocks(cls, version: str = "v2"):
+        return list(cls._get_dist(version).keys())
+
+    @classmethod
+    def get_block(cls, block_id: str, version: str = "v2"):
+        for block in cls.all_blocks(version):
+            if block.ID == block_id:
+                return block
+        raise ValueError("No {} block type".format(block_id))
+
+    @classmethod
+    def block_probability(cls, version: str = "v2"):
+        return list(cls._get_dist(version).values())
+
+    @classmethod
+    def _get_dist(cls, version: str):
+        return cls.BLOCK_TYPE_DISTRIBUTION_DATASET
