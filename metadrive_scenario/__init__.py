f1bee69f2ae53df69eac070be090e7872510f175
Quanyi Li <785878978@qq.com>
2022-10-31 01:44:06 +0000

    Revert "Scenario"

--- metadrive_scenario/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-METADRIVE_SCENARIO_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-METADRIVE_SCENARIO_DATASET_DIR = os.path.join(METADRIVE_SCENARIO_ROOT_DIR, "dataset")
-
-
-class DataType:
-    SYNTHETIC = "synthetic"
