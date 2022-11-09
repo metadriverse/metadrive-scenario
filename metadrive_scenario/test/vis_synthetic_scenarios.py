07ef6894f0517c8fc1863eb6f8e25505fbfb225b
QuanyiLi <quanyili0057@gmail.com>
2022-11-09 15:32:38 +0000

    fix bug

--- metadrive_scenario/test/vis_synthetic_scenarios.py
+++ metadrive_scenario/test/vis_synthetic_scenarios.py
@@ -2,9 +2,10 @@ from metadrive_scenario.utils.env_create_utils import create_env
 from metadrive.policy.idm_policy import IDMPolicy
 
 if __name__ == "__main__":
-    env = create_env("env_num_20_start_seed_0_synthetic",
+    env_class, config = create_env("env_num_20_start_seed_0_synthetic",
                      extra_env_config={"use_render": True, "manual_control": False, "agent_policy":IDMPolicy})
     # env.reset()
+    env = env_class(config)
     for i in range(9,20):
         env.reset()
         for t in range(1000):
--- metadrive_scenario/utils/env_create_utils.py
+++ metadrive_scenario/utils/env_create_utils.py
@@ -1,13 +1,15 @@
 import logging
+from metadrive.utils.config import Config
 import copy
 import os.path as osp
 import pickle
 import numpy as np
 from metadrive_scenario import METADRIVE_SCENARIO_DATASET_DIR
+import json
 
 
 class MetaDriveScenario:
-    def __init__(self, env, scenarios, scenario_start=None, scenario_end=None, seed=0):
+    def __init__(self, config):
         """
         :param env: env to wrap
         :param scenarios: scenario dataset
@@ -15,7 +17,15 @@ class MetaDriveScenario:
         :param scenario_end: scenario end index exclusive)
         :param seed: random seed for determing random choosing scenario
         """
-        self._env = env
+
+        env_class = config["env_class"]
+        env_config = config["env_config"]
+        scenarios = config["scenarios"]
+        scenario_start = config["scenario_start"]
+        scenario_end = config["scenario_end"]
+        seed = config["seed"]
+
+        self._env = env_class(copy.deepcopy(env_config))
         self._scenarios = scenarios
         self._random_seed_for_wrapper = 0
         self.scenario_start = scenario_start or min(list(scenarios.keys()))
@@ -25,6 +35,7 @@ class MetaDriveScenario:
         self._np_random = np.random.RandomState(self._random_seed_for_wrapper)
         self.reset()
         self._env.engine.accept("r", self.reset)
+        self.log_info(copy.deepcopy(env_config))
 
     def __getattr__(self, item):
         if item != "reset":
@@ -42,6 +53,14 @@ class MetaDriveScenario:
         self._env.config["only_reset_when_replay"] = True
         return self._env.reset(force_seed=seed)
 
+    def log_info(self, env_config):
+        if isinstance(env_config, Config):
+            env_config = env_config.get_dict()
+        if "block_dist_config" in env_config:
+            env_config["block_dist_config"] = env_config["block_dist_config"].get_config()
+        print("Environment Config: \n {}".format(json.dumps(env_config, indent=4)))
+        print("Index Range: {}-{}".format(self.scenario_start, self.scenario_end))
+
 
 def key_check(data_1, data_2):
     assert isinstance(data_1, dict) and isinstance(data_2, dict), "Only Dict type can be checked"
@@ -51,7 +70,7 @@ def key_check(data_1, data_2):
                                                                            [(i, data_2[i]) for i in intersect]))
 
 
-def create_env(dataset_name, scenario_start=None, scenario_end=None, extra_env_config=None):
+def create_env(dataset_name, scenario_start=None, scenario_end=None, extra_env_config=None, seed=0):
     extra_env_config = extra_env_config or {}
     if dataset_name.rfind(".pkl") == -1:
         dataset_name += ".pkl"
@@ -60,31 +79,33 @@ def create_env(dataset_name, scenario_start=None, scenario_end=None, extra_env_c
     with open(data_path, "rb+") as file:
         dataset = pickle.load(file)
     env_class = dataset["env_class"]
-    config = dataset["config"]
-    config["record_episode"] = False
-    config["replay_episode"] = None
-    config["only_reset_when_replay"] = True
-    config.update(extra_env_config)
-    config["target_vehicle_configs"] = {}  # this will be filled automatically
-    env = MetaDriveScenario(env_class(config),
-                            scenarios=dataset["scenarios"],
-                            scenario_start=scenario_start,
-                            scenario_end=scenario_end)
+    env_config = dataset["config"]
+    env_config["record_episode"] = False
+    env_config["replay_episode"] = None
+    env_config["only_reset_when_replay"] = True
+    env_config.update(extra_env_config)
+    env_config["target_vehicle_configs"] = {}  # this will be filled automatically
+    config = dict(env_class=env_class,
+                  env_config=env_config,
+                  scenarios=dataset["scenarios"],
+                  scenario_start=scenario_start,
+                  scenario_end=scenario_end,
+                  seed=0)
     print("Load Dataset: {}".format(data_path))
-    print("Environment Config: {}".format(config))
-    print("Index Range: {}-{}".format(env.scenario_start, env.scenario_end))
-    print("Dataset Features: {}".format(dataset["stat"]))
-    return env
+    print("Dataset Features: {}".format(json.dumps(dataset["stat"], indent=4)))
+    return MetaDriveScenario, config
 
 
 if __name__ == "__main__":
-    env = create_env("env_num_20_start_seed_0_synthetic",
-                     extra_env_config={"use_render": True, "manual_control": True})
+    env_class, config = create_env("env_num_20_start_seed_0_synthetic",
+                                   extra_env_config={"use_render": True, "manual_control": True})
+    env = env_class(config)
     env.reset()
     for i in range(20):
         env.reset(seed=i)
         for t in range(100):
             o, r, d, i = env.step([0, 1])
-            print(env.vehicle.position)
+            env.render(text={"seed": env.current_seed})
+            # print(env.vehicle.position)
             if d:
                 break
