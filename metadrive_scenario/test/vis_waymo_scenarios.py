38361bbe091ffa669a2703935fb62683187ffce6
Quanyi Li <quanyili0057@gmail.com>
2022-12-05 19:43:38 +0000

    rebuild dataset (#12)

--- metadrive_scenario/test/vis_waymo_scenarios.py
+++ metadrive_scenario/test/vis_waymo_scenarios.py
@@ -1,18 +1,20 @@
 from metadrive_scenario.utils.env_create_utils import create_env_and_config
 from metadrive.policy.idm_policy import IDMPolicy
+from metadrive.policy.replay_policy import ReplayEgoCarPolicy
 
 if __name__ == "__main__":
     env_class, config = create_env_and_config(
-        "env_num_1165_waymo",
+        "new_1165",
         extra_env_config={
             "use_render": False,
-            "manual_control": True,
-            "replay": False
+            "manual_control": False,
+            "replay": True,
+            "agent_policy" : ReplayEgoCarPolicy,
         },
         random_set_seed_when_reset=False,
-        scenario_start=629,
+        scenario_start=1112,
         waymo_env=True,
-        scenario_end=630
+        scenario_end=1113
     )
     env = env_class(config)
     for i in range(10000):
